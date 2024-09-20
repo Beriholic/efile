f1bbde9bb4a10417002d0fa6f6ea0dbfb1c0ce82
Beriholic <w2h34d@Beriholics-MacBook-Pro.local>
2024-09-20 10:29:25 +0800

    add verison cmd

---
+++ cmd/versionCmd.go
@@ -0,0 +1,18 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+)
+
+var versionNumber = "0.1.1"
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Print the version number of efile",
+	Long:  `Print the version number of efile.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("efile version %s\n", versionNumber)
+	},
+}
